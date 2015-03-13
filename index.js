2d59daeba8482581ed39a06dbdc9fe8e66522560
zhi.zhong <zhi.zhong@163.com>
2015-03-13 22:41:38 +0800

    Add els

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+var views = require('co-views');
 var app = require('koa')();
 var router = require('koa-router')();
 
 var PORT = 3001;
+var render = views(__dirname + '/refs', { ext: 'ejs' });
 
 router.get('/', function *(next) {
-  this.body = 'Hello world';
+  // this.body = 'Hello world';
+  this.body = yield render('index', {
+    title: 'test',
+    description: 'xxx',
+    body: '<h1>Hello EJS</h1>'
+  });
 });
 
 app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(PORT);
-
-console.log('server run at http://localhost:' + PORT);
+if (!module.parent) {
+  app.listen(PORT);
+  console.log('server run at http://localhost:' + PORT);
+}
