730eca55ffeb8bb519480ed5471c07e382be6c19
zhi.zhong <zhi.zhong@163.com>
2015-10-17 23:50:07 +0800

    Remove copyright

--- api/hotel/hotelList.js
+++ api/hotel/hotelList.js
@@ -19,7 +19,7 @@ module.exports = function(self) {
         "id": 1,
         "name": "北京京仪大酒店",
         "address": "北京市海淀区大钟寺东路9号",
-        "image": "http://userimg.qunar.com/imgs/201410/25/GIGWX47MHTgwYKxg776.jpg",
+        "image": "http://userimg.xxx.com/imgs/201410/25/GIGWX47MHTgwYKxg776.jpg",
         "star": "高档",
         "price": 100
       },
@@ -27,7 +27,7 @@ module.exports = function(self) {
         "id": 2,
         "name": "7天酒店北京南宫温泉店",
         "address": "北京市丰台区王佐镇长青路87号",
-        "image": "http://userimg.qunar.com/imgs/201411/10/66I5P26nk1fGZGKq676.jpg",
+        "image": "http://userimg.xxx.com/imgs/201411/10/66I5P26nk1fGZGKq676.jpg",
         "star": "经济型酒店",
         "price": 200
       },
@@ -35,7 +35,7 @@ module.exports = function(self) {
         "id": 3,
         "name": "北京四季御园国际大酒店",
         "address": "北京海淀区北旱河路168号-3号(西北五环香泉环岛)",
-        "image": "http://userimg.qunar.com/imgs/201209/03/Z7S8IZZ-0oFHXwnMZ76.jpg",
+        "image": "http://userimg.xxx.com/imgs/201209/03/Z7S8IZZ-0oFHXwnMZ76.jpg",
         "star": "三星级",
         "price": 300
       },
@@ -43,7 +43,7 @@ module.exports = function(self) {
         "id": 4,
         "name": "北京斯洛主题酒店南站店",
         "address": "北京市丰台区马家堡东路71号",
-        "image": "http://userimg.qunar.com/imgs/201411/10/GIGWX47OD7Q2GRkO776.jpg",
+        "image": "http://userimg.xxx.com/imgs/201411/10/GIGWX47OD7Q2GRkO776.jpg",
         "star": "四星级酒店",
         "price": 400
       },
@@ -51,7 +51,7 @@ module.exports = function(self) {
         "id": 5,
         "name": "北京方圆四季酒店酒仙桥店",
         "address": "北京朝阳区酒仙桥三街坊10号楼",
-        "image": "http://userimg.qunar.com/imgs/201112/07/Z73ZyZ9Dm1hhspgDZ76.jpg",
+        "image": "http://userimg.xxx.com/imgs/201112/07/Z73ZyZ9Dm1hhspgDZ76.jpg",
         "star": "五星级酒店",
         "price": 500
       }
